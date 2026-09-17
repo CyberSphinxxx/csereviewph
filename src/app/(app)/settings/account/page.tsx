58abb7bda56bb5acd365809bd9526200c1d4f1a8
CyberSphinxxx <johnlemargonzales@gmail.com>
2026-09-17 19:06:01 +0800

    feat(appsettingsaccount): update page.tsx

--- src/app/(app)/settings/account/page.tsx
+++ src/app/(app)/settings/account/page.tsx
@@ -34,7 +34,7 @@ export default function AccountSettingsPage() {
       const url = URL.createObjectURL(blob);
       const a = document.createElement("a");
       a.href = url;
-      a.download = `csereviewph-cloud-account-data-${new Date().toISOString().slice(0, 10)}.json`;
+      a.download = `reviewtayo-cloud-account-data-${new Date().toISOString().slice(0, 10)}.json`;
       document.body.appendChild(a);
       a.click();
       document.body.removeChild(a);
